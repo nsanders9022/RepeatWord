00b736e801d1b71f58d58db7ca002306432ddd6c
Nicole Sanders <nsanders9022@gmail.com>
2017-02-17 08:57:58 -0800

    add properties and a constructor to the class as well as add a method that passes the first spec

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -5,8 +5,33 @@ namespace RepeatCounterApp.Objects
 {
   public class RepeatCounter
   {
+    private string _sentence;
+    private string _word;
 
+    public RepeatCounter(string sentence, string word)
+    {
+      _sentence = sentence;
+      _word = word;
+    }
 
+    public string GetSentence()
+    {
+      return _sentence;
+    }
 
+    public string GetWord()
+    {
+      return _word;
+    }
+
+    public int CountRepeats(string sentenceInput, string wordInput)
+    {
+      int result = 0;
+      if (sentenceInput == wordInput)
+      {
+        result += 1;
+      }
+      return result;
+    }
   }
 }
